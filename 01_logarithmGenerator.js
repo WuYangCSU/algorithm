bac60fb54d6bd0a574185fa3de098d45e59d86f0
WuYangjava <1224181303@qq.com>
2021-03-30 10:09:51 +0800

    添加归并排序

--- 01_logarithmGenerator.js
+++ 01_logarithmGenerator.js
@@ -51,3 +51,30 @@ export function checking2(func){
       console.log("两数组相等，算法正确。");
       return true 
 }
+
+// 归并算法使用
+export function checking3(func){
+      for(let i = 0; i<100; i++){
+            let arr1 = new Array(Math.round(Math.random()*100)).fill(0)
+            arr1.forEach((value,index)=>{
+                 arr1[index]=Math.round(Math.random()*100)
+            })
+            let arr2 =arr1.slice()
+            arr1.sort((a,b)=>{
+                  return a-b
+            })
+            console.log("数组一："+arr1+"；");  
+           
+            func(arr2,0,arr2.length-1)
+            console.log("数组二（方法一）："+arr2+"；")
+            for( let i=0;i<arr1.length;i++){
+                  if(arr1[i]!==arr2[i]){
+                        console.log("两数组不相等，算法不正确。");
+                        return false
+                  }
+            }
+      }
+     
+      console.log("两数组相等，算法正确。");
+      return true 
+}
